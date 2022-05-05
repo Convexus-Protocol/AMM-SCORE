451404e0e7c9e1195842fbfff54c8b671ddd9afb
ICONationDevTeam <iconation.team@gmail.com>
2022-05-05 16:45:33 +0200

    Improve run.py with dynamic password

--- run.py
+++ run.py
@@ -7,15 +7,14 @@ from scripts.config import Config
 from scripts.deploy_contract import deploy, update
 from scripts.call_contract import invoke, call
 from scripts.meta import get_meta
+from os.path import exists
+import getpass
 
 class Command:
 
     def __init__(self) -> None:
         parser = argparse.ArgumentParser()
         parser.add_argument('-e', '--endpoint', type=str, default='gochain', help='target endpoint for connection')
-        parser.add_argument('-k', '--keystore', type=argparse.FileType('r'), default='./scripts/config/keystores/sejong/operator.icx',
-                            help='keystore file for creating transactions')
-        parser.add_argument('-p', '--password', type=str, default='gochain', help='keystore password')
         subparsers = parser.add_subparsers(title='Available commands', dest='command')
         subparsers.required = True
 
@@ -40,6 +39,19 @@ class Command:
         getattr(self, args.command)(args)
 
     @staticmethod
+    def get_keystore_path(network):
+        return f"scripts/config/keystores/{network}/operator.icx"
+
+    @staticmethod
+    def get_keystore_password(network):
+        target_path = f"scripts/config/keystores/{network}/operator.pwd"
+
+        if exists(target_path):
+            return open(target_path, "r").read()
+        else:
+            return getpass.getpass(prompt='Enter the keystore password: ')
+
+    @staticmethod
     def optimizedJar(args):
         javaPkg, version, build = get_meta(args.package, args.endpoint)
         print(f"Executing {javaPkg}:build ...")
@@ -53,25 +65,33 @@ class Command:
     def deploy(args):
         print(f" -------------------- Deploying {args.package} ... -------------------- ")
         Command.optimizedJar(args)
-        config = Config(args.endpoint, args.keystore.name, args.password)
+        keystore_path = Command.get_keystore_path(args.endpoint)
+        keystore_password = Command.get_keystore_password(args.endpoint)
+        config = Config(args.endpoint, keystore_path, keystore_password)
         deploy(config, args.package)
 
     @staticmethod
     def update(args):
         print(f" -------------------- Updating {args.package} ... -------------------- ")
         Command.optimizedJar(args)
-        config = Config(args.endpoint, args.keystore.name, args.password)
+        keystore_path = Command.get_keystore_path(args.endpoint)
+        keystore_password = Command.get_keystore_password(args.endpoint)
+        config = Config(args.endpoint, keystore_path, keystore_password)
         update(config, args.package)
 
     @staticmethod
     def invoke(args):
         print(f" -------------------- Invoking {args.package} ... -------------------- ")
-        config = Config(args.endpoint, args.keystore.name, args.password)
+        keystore_path = Command.get_keystore_path(args.endpoint)
+        keystore_password = Command.get_keystore_password(args.endpoint)
+        config = Config(args.endpoint, keystore_path, keystore_password)
         invoke(config, args.package, args.params)
 
     @staticmethod
     def call(args):
-        config = Config(args.endpoint, args.keystore.name, args.password)
+        keystore_path = Command.get_keystore_path(args.endpoint)
+        keystore_password = Command.get_keystore_password(args.endpoint)
+        config = Config(args.endpoint, keystore_path, keystore_password)
         print(call(config, args.package, args.params))
 
 if __name__ == "__main__":
