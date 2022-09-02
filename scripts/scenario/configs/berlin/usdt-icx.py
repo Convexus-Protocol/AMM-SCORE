8176054b7cddbea825840074342f15a0003becb4
ICONationDevTeam <iconation.team@gmail.com>
2022-09-02 21:00:43 +0200

    Add USDT ICX pool

---
+++ scripts/scenario/configs/berlin/usdt-icx.py
@@ -0,0 +1,45 @@
+import json
+
+USDT = {
+  "address": "cx16dcf3e6a0e7fce4a8624a82d55f4efe4426444c",
+  "amount": int(0.60 * 10**6)
+}
+
+ICX = {
+  "address": "cx0000000000000000000000000000000000000001",
+  "amount": 10**18
+}
+
+token0, token1 = sorted([USDT, ICX], key=lambda token: token['address'])
+
+config = {
+  # Type: String
+  # "network" value must be:
+  #   - "lisbon" for Lisbon Network,
+  #   - "berlin" for Berlin Network,
+  #   - "custom" for the custom Convexus Network
+  #   - "mainnet" for MainNet Network,
+  "network": "berlin",
+
+  "pool": {
+    # Type: Address
+    # token0 address
+    "token0": token0['address'],
+    # Type: BigInteger
+    # Amount0 for initialization
+    "amount0": token0['amount'],
+
+    # Type: Address
+    # token1 address
+    "token1": token1['address'],
+    # Type: BigInteger
+    # Amount1 for initialization
+    "amount1": token1['amount'],
+
+    # Type: Integer
+    "fee": 3000
+  }
+}
+
+# This script must only print the configuration in JSON format, nothing else
+print(json.dumps(config))
