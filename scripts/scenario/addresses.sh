0a43d0d878f9d3f68403b041d3dc54c0d10ae799
ICONationDevTeam <iconation.team@gmail.com>
2022-10-14 13:15:35 +0200

    Modify output addresses script

--- scripts/scenario/addresses.sh
+++ scripts/scenario/addresses.sh
@@ -11,33 +11,24 @@ fi
 endpoint=$1
 
 echo "export const DEFAULT_BOOKMARK = {"
-echo "  \"$(getAddress "core/factory" ${endpoint})\": \"Factory\","
-
-if [ ${endpoint} == "berlin" ] ; then
-  echo "  \"$(getAddress core/pools/berlin/bnusd-icx/pool ${endpoint})\": \"BNUSD/ICX Pool\","
-  echo "  \"$(getAddress core/pools/berlin/bnusd-usdc/pool ${endpoint})\": \"BNUSD/USDC Pool\","
-  echo "  \"$(getAddress core/pools/berlin/usdc-icx/pool ${endpoint})\": \"USDC/ICX Pool\","
-  echo "  \"$(getAddress core/pools/berlin/usdc-sicx/pool ${endpoint})\": \"USDC/SICX Pool\","
-  echo "  \"$(getAddress core/pools/berlin/usdt-icx/pool ${endpoint})\": \"USDT/ICX Pool\","
-  echo "  \"$(getAddress core/pools/berlin/weth-crv/pool ${endpoint})\": \"WETH/CRV Pool\","
-fi
-
+echo "  \"$(getAddress "$(getFactoryPkg)" ${endpoint})\": \"Factory\","
+echo "  \"$(getAddress core/pools/berlin/bnusd-icx/pool ${endpoint})\": \"BNUSD/ICX Pool\","
+echo "  \"$(getAddress core/pools/berlin/bnusd-usdc/pool ${endpoint})\": \"BNUSD/USDC Pool\","
+echo "  \"$(getAddress core/pools/berlin/usdc-icx/pool ${endpoint})\": \"USDC/ICX Pool\","
+echo "  \"$(getAddress core/pools/berlin/usdc-sicx/pool ${endpoint})\": \"USDC/SICX Pool\","
+echo "  \"$(getAddress core/pools/berlin/usdt-icx/pool ${endpoint})\": \"USDT/ICX Pool\","
+echo "  \"$(getAddress core/pools/berlin/weth-crv/pool ${endpoint})\": \"WETH/CRV Pool\","
 echo "  \"$(getAddress $(getSwapRouterPkg) ${endpoint})\": \"Swap Router\","
 echo "  \"$(getAddress $(getNonfungibleTokenPositionDescriptorPkg) ${endpoint})\": \"Position Descriptor\","
 echo "  \"$(getAddress $(getNonFungiblePositionManagerPkg) ${endpoint})\": \"Position Manager\","
-echo "  \"$(getAddress $(getReadOnlyPool) ${endpoint})\": \"Pool ReadOnly\","
-echo "  \"$(getAddress $(getQuoter) ${endpoint})\": \"Quoter\","
+echo "  \"$(getAddress $(getReadOnlyPoolPkg) ${endpoint})\": \"Pool ReadOnly\","
+echo "  \"$(getAddress $(getQuoterPkg) ${endpoint})\": \"Quoter\","
 echo "  \"$(getAddress $(getPoolInitializerPkg) ${endpoint})\": \"Pool Initializer\","
-
-if [ ${endpoint} == "berlin" ] ; then
-  echo "  \"$(getAddress tokens/bnUSD-token ${endpoint})\": \"bnUSD\","
-  echo "  \"$(getAddress tokens/CRV-token ${endpoint})\": \"CRV\","
-  echo "  \"$(getAddress tokens/CTT-token ${endpoint})\": \"CTT\","
-  echo "  \"$(getAddress tokens/sICX-token ${endpoint})\": \"sICX\","
-  echo "  \"$(getAddress tokens/USDC-token ${endpoint})\": \"USDC\","
-  echo "  \"$(getAddress tokens/USDT-token ${endpoint})\": \"USDT\","
-  echo "  \"$(getAddress tokens/WETH-token ${endpoint})\": \"WETH\","
-fi
-
+echo "  \"$(getAddress tokens/bnUSD-token ${endpoint})\": \"bnUSD\","
+echo "  \"$(getAddress tokens/CRV-token ${endpoint})\": \"CRV\","
+echo "  \"$(getAddress tokens/CTT-token ${endpoint})\": \"CTT\","
+echo "  \"$(getAddress tokens/sICX-token ${endpoint})\": \"sICX\","
+echo "  \"$(getAddress tokens/USDC-token ${endpoint})\": \"USDC\","
+echo "  \"$(getAddress tokens/USDT-token ${endpoint})\": \"USDT\","
+echo "  \"$(getAddress tokens/WETH-token ${endpoint})\": \"WETH\","
 echo "}"
-
